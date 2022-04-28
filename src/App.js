93838f86a4fb059e79dd253c3b77aa4a49c59474
dhilipsiva <dhilipsiva@pm.me>
2022-04-28 12:36:22 +0530

    keycloak integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+import RenderOnAnonymous from "./components/RenderOnAnonymous";
+import RenderOnAuthenticated from "./components/RenderOnAuthenticated";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <RenderOnAnonymous>
+          <div className="App">NOT Authenticated yet!</div>
+        </RenderOnAnonymous>
+        <RenderOnAuthenticated>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </RenderOnAuthenticated>
+      </div>
+    </BrowserRouter>
   );
 }
 
